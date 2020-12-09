20a267e5ab9684f57e75bcede23bc2ace9b9ba9a
Brian Shapiro <briandavidshapiro@gmail.com>
2020-12-09 08:50:06 -0500

    break my build

--- operations.js
+++ operations.js
@@ -1,14 +1,14 @@
 function add(a = 0, b = 0) {
-  return a + b;
+	return a + b + 1;
 }
 
 function average(...numbers) {
-  let total = 0;
-  if (numbers.length === 0) return 0;
-  for (let num of numbers) {
-    total += num;
-  }
-  return total / numbers.length;
+	let total = 0;
+	if (numbers.length === 0) return 0;
+	for (let num of numbers) {
+		total += num;
+	}
+	return total / numbers.length;
 }
 
 module.exports = { add, average };
